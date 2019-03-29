bfb6299e86b76959657b16e3f20f5070c21af88c
Joel Ye <joelye9@gmail.com>
2019-03-29 09:45:30 -0400

    Csv Import Wizard (#5)
    * prepare for csv parse
    
    * Complete CSV wizard with basic header heuristic match
    
    * add money parsing, convert tag color, trim strings
    
    * change copy and redirect

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import {ToastProvider} from 'react-toast-notifications';
 import HomeContainer from "./components/HomeContainer";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import CSVWizard from "./components/csv/CSVWizard";
 import ItemWrapper from "./components/item/ItemWrapper";
 
 class App extends Component {
@@ -19,6 +20,7 @@ class App extends Component {
                         <Navigation/>
                         <Switch>
                             <Route path="/" exact component={HomeContainer}/>
+                            <Route path="/admin/csv" component={CSVWizard}/>
                             <Route path="/item" component={ItemWrapper}/>
                             <Route component={HomeContainer}/>
                         </Switch>
