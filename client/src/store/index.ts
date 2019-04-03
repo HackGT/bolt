8f9f1b64a99c0abc403adef4fefe3efbf70b28e7
Evan Strat <evan10s@users.noreply.github.com>
2019-04-02 22:26:00 -0400

    Fix issue where React Devtools broke page if React Devtools extension was not enabled/installed

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -2,10 +2,16 @@ import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import reducers from '../reducers/reducers';
 import thunk from 'redux-thunk'
 
+//thanks https://github.com/reduxjs/redux/issues/2359#issuecomment-372609400
+const composeSetup =
+    process.env.NODE_ENV !== 'production' && typeof window === 'object' &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
+
 export const store = createStore(
     reducers,
-    compose(
+    composeSetup(
         applyMiddleware(thunk) as any,
-        (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+
     )
 );
