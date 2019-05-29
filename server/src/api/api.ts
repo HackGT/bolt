d4060ecfb5bb8f1570dd48ef9fbff639c4e31598
Evan Strat <evan10s@users.noreply.github.com>
2019-05-28 22:10:25 -0500

    Stubbed createItem GQL endpoint

--- server/src/api/api.ts
+++ server/src/api/api.ts
@@ -5,14 +5,19 @@ import * as path from "path";
 import express from "express";
 import graphqlHTTP from "express-graphql";
 import {buildSchema, GraphQLError} from "graphql";
-import {QueryResolvers} from "./api.graphql";
+import {MutationResolvers, QueryResolvers} from "./api.graphql";
 import {DB} from "../database";
 import {isAdminNoAuthCheck} from "../auth/auth";
 
 const schemaFile = path.join(__dirname, "./api.graphql");
-const schema = buildSchema(fs.readFileSync(schemaFile, { encoding: "utf8" }));
+const schema = buildSchema(fs.readFileSync(schemaFile, {encoding: "utf8"}));
 
 export const apiRoutes = express.Router();
+
+// interface IResolver {
+//     Query: QueryResolvers<express.Request>;
+//     Mutation: MutationResolvers<express.Request>;
+// }
 
 // Helper function for incorrect typedefs
 // express-graphql does not have a `root` parameter as the first argument but code-gen puts it there anyway
@@ -24,10 +29,18 @@ function fixArguments<A, B, C>(fakeRoot: A, fakeArgs: B, fakeContext: C): { args
 }
 
 
-const resolvers: QueryResolvers<express.Request> = {
+const resolvers: QueryResolvers<express.Request> | MutationResolvers<express.Request> = {
+    /* Queries */
+    /**
+     * Returns information about the current signed in user
+     * Access level: current signed in user
+     * @param root
+     * @param _args
+     * @param _context
+     */
     user: async (root, _args, _context) => {
         // @ts-ignore
-        const { args, context } = fixArguments(root, _args, _context);
+        const {args, context} = fixArguments(root, _args, _context);
         return {
             uuid: context.user.uuid,
             name: context.user.name,
@@ -38,9 +51,17 @@ const resolvers: QueryResolvers<express.Request> = {
             admin: context.user.admin
         };
     },
+    /**
+     * Returns information about a single item
+     * TODO: better filtering/search
+     * Access level: any signed in user
+     * @param root
+     * @param _args
+     * @param _context
+     */
     item: async (root, _args, _context) => {
         // @ts-ignore
-        const { args, context} = fixArguments(root, _args, _context);
+        const {args, context} = fixArguments(root, _args, _context);
 
         if (args.id <= 0) {
             throw new GraphQLError("Invalid item ID.  The item ID you provided was <= 0, but item IDs must be >= 1.");
@@ -48,7 +69,7 @@ const resolvers: QueryResolvers<express.Request> = {
 
         let item = await DB.from("items")
             .join("categories", "items.category_id", "=", "categories.category_id")
-            .where({ item_id: args.id });
+            .where({item_id: args.id});
 
         if (item.length === 0) {
             return null;
@@ -70,9 +91,17 @@ const resolvers: QueryResolvers<express.Request> = {
             ...item
         };
     },
-    items:  async (root, _args, _context) => {
+    /**
+     * Bulk items API
+     * TODO: pagination/returned quantity limit
+     * Access level: any signed in user
+     * @param root
+     * @param _args
+     * @param _context
+     */
+    items: async (root, _args, _context) => {
         // @ts-ignore
-        const { args, context} = fixArguments(root, _args, _context);
+        const {args, context} = fixArguments(root, _args, _context);
         const items = await DB.from("items")
             .join("categories", "items.category_id", "=", "categories.category_id");
         console.log(items);
@@ -85,7 +114,41 @@ const resolvers: QueryResolvers<express.Request> = {
                 ...item
             };
         });
-    }
+    },
+
+    /* Mutations */
+    /**
+     * Create a new item
+     * TODO: implement
+     * Access level: admins
+     * @param root
+     * @param _args
+     * @param _context
+     */
+    createItem: async (root, _args, _context) => {
+        // @ts-ignore
+        const {args, context} = fixArguments(root, _args, _context);
+
+        // Restrict endpoint to admins
+        if (!context.user.admin) {
+            throw new GraphQLError("You do not have permission to access the createItem endpoint");
+        }
+
+        return {
+            id: 3,
+            name: "Arduino",
+            description: "",
+            imageUrl: "",
+            category: "A",
+            totalAvailable: 3,
+            maxRequestQty: 3,
+            price: 3,
+            hidden: true,
+            returnRequired: true,
+            approvalRequired: true,
+            owner: ""
+        };
+    },
 };
 
 apiRoutes.post("/", graphqlHTTP({
