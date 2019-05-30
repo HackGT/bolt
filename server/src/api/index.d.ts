2bca9c9d5d88d2bd08eec139da450fccbedbcfd2
Evan Strat <evan10s@users.noreply.github.com>
2019-05-29 23:13:24 -0500

    Revert supposed fix for importing .graphql files

--- server/src/api/index.d.ts
+++
@@ -1,9 +0,0 @@
-// See https://github.com/apollographql/graphql-tag/issues/59#issuecomment-316991007
-// for why this is here
-// tl;dr This ensures that importing .graphql files in TypeScript works correctly
-declare module "*.graphql" {
-    import {DocumentNode} from "graphql";
-
-    const value: DocumentNode;
-    export = value;
-}
