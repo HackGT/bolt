ab0a7a676066329de052792ee13b8d6d1054907a
Evan Strat <evan10s@users.noreply.github.com>
2019-06-16 00:17:11 -0500

    Added Apollo Mutation component and fixed up input onChange methods

--- client/src/components/item/AddOptionDropdown.tsx
+++ client/src/components/item/AddOptionDropdown.tsx
@@ -1,41 +1,46 @@
-import React, {Component} from 'react';
-import {Dropdown} from "semantic-ui-react";
+import React, {Component} from "react";
+import {Dropdown, DropdownProps} from "semantic-ui-react";
 
 interface AddOptionDropdownProps {
-    name: string,
-    required: boolean,
-    placeholder: string,
-    options: Option[]
+    name: string;
+    required: boolean;
+    placeholder: string;
+    options: Option[];
+    onChange: (event: React.SyntheticEvent<HTMLElement>, data: DropdownProps, inputName: string) => void;
 }
 
 export interface Option {
-    text: string,
-    value: string
+    text: string;
+    value: string;
 }
 
 interface AddOptionDropdownState {
-    options: Option[],
-    currentValue: string
+    options: Option[];
+    currentValue?: any;
 }
 
 class AddOptionDropdown extends Component<AddOptionDropdownProps, AddOptionDropdownState> {
     constructor(props: AddOptionDropdownProps) {
         super(props);
-        this.handleAddition = this.handleAddition.bind(this);
-        this.handleChange = this.handleChange.bind(this);
         this.state = {
             options: this.props.options,
             currentValue: ""
         };
     }
 
-    handleAddition = (e: any, {value}: any) => {
+    public handleAddition = (e: any, {value}: any) => {
         this.setState({
             options: [{text: value, value}, ...this.state.options],
-        })
-    };
+        });
+    }
 
-    handleChange = (e: any, {value}: any) => this.setState({currentValue: value});
+    public handleChange = (event: React.SyntheticEvent<HTMLElement>, data: DropdownProps) => {
+        console.log(event, data.value);
+        this.setState({
+            currentValue: data.value
+        });
+        this.props.onChange(event, data, this.props.name);
+    };
     // onAddItem(e: React.KeyboardEvent<HTMLElement>, dropdown: DropdownProps): void {
     //     const value: string = dropdown.
     //     this.setState({
@@ -43,7 +48,7 @@ class AddOptionDropdown extends Component<AddOptionDropdownProps, AddOptionDropd
     //     });
     // }
 
-    render() {
+    public render() {
         return (
             <Dropdown width={4}
                       type="text"
--- client/src/components/item/ItemEditForm.tsx
+++ client/src/components/item/ItemEditForm.tsx
@@ -1,10 +1,14 @@
 import React, {ChangeEvent, Component, FormEvent} from "react";
-import {Item} from "../inventory/HardwareItem";
+import {ItemNoId} from "../inventory/HardwareItem";
 import {Button, CheckboxProps, DropdownProps, Form, Label, Popup} from "semantic-ui-react";
 import AddOptionDropdown from "./AddOptionDropdown";
+import Mutation from "react-apollo/Mutation";
+import {withToastManager} from "react-toast-notifications";
+import gql from "graphql-tag";
+import {Redirect} from "react-router";
 
 interface ItemDetails {
-    requireApproval: boolean;
+    approvalRequired: boolean;
     returnRequired: boolean;
     price: number;
     hidden: boolean;
@@ -14,9 +18,12 @@ interface ItemDetails {
 interface ItemEditProps {
     itemId?: number;
     createItem: boolean;
+    toastManager: any;
 }
 
-export type ItemComplete = Item & ItemDetails;
+export type ItemComplete = ItemNoId & ItemDetails & {
+    [name: string]: any | any[];
+};
 
 interface ItemEditState {
     loading: boolean;
@@ -25,16 +32,36 @@ interface ItemEditState {
     [name: string]: any | any[];
 }
 
+const CREATE_ITEM = gql`
+    mutation createItem ($newItem: ItemInput!) {
+        createItem(newItem: $newItem) {
+            id
+            item_name
+        }
+    }
+`;
+
+const UPDATE_ITEM = CREATE_ITEM;
+
 class ItemEditForm extends Component<ItemEditProps, ItemEditState> {
     constructor(props: ItemEditProps) {
         super(props);
         this.state = {
-            "loading": !this.props.createItem,
-            "item": null,
-            "item-return-required": true,
-            "item-approval-required": true,
-            "item-hidden": false,
-            "itemOwnerChoices": [
+            loading: !this.props.createItem,
+            item: {
+                item_name: "",
+                description: "",
+                imageUrl: "",
+                category: "",
+                totalAvailable: 0,
+                maxRequestQty: 0,
+                price: 0,
+                approvalRequired: true,
+                returnRequired: true,
+                hidden: false,
+                owner: "HackGT"
+            },
+            itemOwnerChoices: [
                 {key: "hackgt", text: "HackGT", value: "HackGT"},
                 {key: "hive", text: "The Hive", value: "The Hive"},
                 {key: "is", text: "Invention Studio", value: "Invention Studio"}]
@@ -45,32 +72,7 @@ class ItemEditForm extends Component<ItemEditProps, ItemEditState> {
     public componentDidMount = (): void => {
         // this.loadItemData(this.props.match.params.itemId);
 
-    };
-
-    public finishedLoadingItemData = () => {
-        this.setState({
-            item: {
-                id: "3a",
-                category: "Microcontrollers",
-                name: "Raspberry Pi 3",
-                totalQty: 50,
-                maxReqQty: 1,
-                qtyRemaining: 45,
-                description: "",
-                imageUrl: "",
-                hidden: false,
-                owner: "HackGT",
-                requireApproval: true,
-                returnRequired: true,
-                price: 35
-            },
-            loading: false,
-        });
-    };
-
-    public loadItemData = (id: string): void => {
-        setTimeout(this.finishedLoadingItemData, 1000);
-    };
+    }
 
     public handleInputChangeCheckbox = (event: FormEvent<HTMLInputElement>, checkboxProps: CheckboxProps): void => {
         console.log(checkboxProps.checked);
@@ -79,14 +81,26 @@ class ItemEditForm extends Component<ItemEditProps, ItemEditState> {
             const value: boolean = checkboxProps.checked;
             const name: string = checkboxProps.name;
 
+            // @ts-ignore
             this.setState({
-                [name]: value
+                item: {
+                    ...this.state.item,
+                    [name]: value
+                }
             });
         }
-    };
+    }
 
-    public handleInputChangeDropdown = (name: string, dropdownProps: DropdownProps): void => {
-        const value = dropdownProps.value;
+    public handleInputChangeDropdown = (event: React.SyntheticEvent<HTMLElement>, data: DropdownProps, inputName: string): void => {
+        const target = event.target;
+        const value = data.value;
+        // @ts-ignore
+        this.setState({
+            item: {
+                ...this.state.item,
+                [inputName]: value
+            }
+        });
     };
 
     public handleInputChange = (event: ChangeEvent<HTMLInputElement>): void => {
@@ -94,10 +108,14 @@ class ItemEditForm extends Component<ItemEditProps, ItemEditState> {
         const value = target.type === "checkbox" ? target.checked : target.value;
         const name = target.name;
 
+        // @ts-ignore
         this.setState({
-            [name]: value
+            item: {
+                ...this.state.item,
+                [name]: value
+            }
         });
-    };
+    }
 
     public render() {
         console.log("itemId is...", this.props.itemId);
@@ -117,93 +135,135 @@ class ItemEditForm extends Component<ItemEditProps, ItemEditState> {
             {key: "invention_studio", text: "Invention Studio", value: "Invention Studio"}];
 
         return (
-            <Form loading={this.state.loading} onChange={this.handleInputChange}>
-                <Form.Group>
-                    <Form.Input width={6}
-                                label="Item name"
-                                type="text"
-                                name="item-name"
-                                required
-                                placeholder="Ventral quark accelerator"/>
-                    <Form.Input width={6}
-                                name="item-img"
-                                label="Image"
-                                type="file"/>
-                </Form.Group>
-                <Form.TextArea width={12}
-                               label="Description"
-                               type="textarea"
-                               name="item-description"
-                               rows={2}
-                               placeholder="Prior to use, dust the dorsal tachyon resistance sensor array."/>
-                <Form.Group>
-                    <Form.Input label="Category"
-                    >
-                        <AddOptionDropdown name="item-category" required={true} placeholder="Spacecraft"
-                                           options={categoryChoices}/>
-                    </Form.Input>
-                    <Form.Input width={4}
-                                label="Item value ($)"
-                                type="number"
-                                name="item-value"
-                                labelPosition="left"
-                                placeholder="42.00">
-                        <Label>$</Label>
-                        <input min={0}/>
-                    </Form.Input>
-                    <Form.Input label="Owner">
-                        <AddOptionDropdown name="item-owner" required={true} placeholder="Owner"
-                                           options={itemOwnerChoices}/>
-                    </Form.Input>
-                </Form.Group>
+            <Mutation mutation={this.props.createItem ? CREATE_ITEM : UPDATE_ITEM}>
+                {(submitForm: any, {loading, error, called, data}: any) => (
+                    <Form loading={this.state.loading || loading} onChange={this.handleInputChange}
+                          onSubmit={e => {
+                              e.preventDefault();
+                              const {toastManager} = this.props;
+                              submitForm({
+                                  variables: {
+                                      newItem: {
+                                          item_name: "Blueberry Pi 2",
+                                          description: "Better than Raspberry",
+                                          imageUrl: "lolno",
+                                          category: "Microcontrollers",
+                                          totalAvailable: 3,
+                                          maxRequestQty: 1,
+                                          price: 29.95,
+                                          approvalRequired: true,
+                                          returnRequired: true,
+                                          hidden: false,
+                                          owner: "HackGT"
+                                      }
+                                  }
+                              }).then(() => {
+                                  toastManager.add("Successfully created ${item_name}", {
+                                      appearance: "success",
+                                      autoDismiss: true,
+                                      placement: "top-center"
+                                  });
+                              }).catch(() => {
+                                  toastManager.add("Unable to create your item :(", {
+                                      appearance: "error",
+                                      autoDismiss: false,
+                                      placement: "top-center"
+                                  });
+                              });
 
-                <h2>Stock</h2>
-                <Form.Group>
-                    <Form.Input width={6}
-                                label="Quantity in stock"
-                                type="number"
-                                name="item-qty-in-stock"
-                                required
-                                placeholder="47">
-                        <input type="number" min={0}/>
-                    </Form.Input>
-                    <Form.Input width={6}
-                                label="Quantity allowed per request"
-                                type="number"
-                                name="item-qty-per-request"
-                                required
-                                placeholder="6">
-                        <input type="number" min={0}/>
-                    </Form.Input>
-                </Form.Group>
-                <Form.TextArea width={6}
-                               label="Serial numbers (one per line)"
-                               name="item-serial-nums"
-                               rows={6}
-                               placeholder="2873-813"/>
 
-                <h2>Checkout Controls</h2>
+                          }}>
 
-                <Popup inverted={true} trigger={<Form.Checkbox name="item-return-required"
-                                                               label="Return required"
-                                                               onChange={this.handleInputChangeCheckbox}
-                                                               defaultChecked/>}
-                       content="Whether users who check out this item are expected to return it"/>
-                <Popup inverted={true} trigger={<Form.Checkbox label="Approval required"
-                                                               name="item-approval-required"
-                                                               onChange={this.handleInputChangeCheckbox}
-                                                               defaultChecked/>}
-                       content="Whether hardware checkout staff must approve requests for this item"/>
-                <Popup inverted={true} trigger={<Form.Checkbox label="Hidden"
-                                                               name="item-hidden"
-                                                               onChange={this.handleInputChangeCheckbox}/>}
-                       content="Whether to hide this item on the public list of hardware"/>
+                        {data && !error ? <Redirect to="/"/> : ""}
+                        <Form.Group>
+                            <Form.Input width={6}
+                                        label="Item name"
+                                        type="text"
+                                        name="item_name"
+                                        required
+                                        placeholder="Ventral quark accelerator"/>
+                            <Form.Input width={6}
+                                        name="item-img"
+                                        label="Image"
+                                        type="file"/>
+                        </Form.Group>
+                        <Form.TextArea width={12}
+                                       label="Description"
+                                       type="textarea"
+                                       name="description"
+                                       rows={2}
+                                       placeholder="Prior to use, dust the dorsal tachyon resistance sensor array."/>
+                        <Form.Group>
+                            <Form.Input label="Category"
+                            >
+                                <AddOptionDropdown name="category" required={true} placeholder="Spacecraft"
+                                                   options={categoryChoices} onChange={this.handleInputChangeDropdown}/>
+                            </Form.Input>
+                            <Form.Input width={4}
+                                        label="Item value ($)"
+                                        type="number"
+                                        name="price"
+                                        labelPosition="left"
+                                        placeholder="42.00">
+                                <Label>$</Label>
+                                <input min={0}/>
+                            </Form.Input>
+                            <Form.Input label="Owner">
+                                <AddOptionDropdown name="owner" required={true} placeholder="Owner"
+                                                   options={itemOwnerChoices}
+                                                   onChange={this.handleInputChangeDropdown}/>
+                            </Form.Input>
+                        </Form.Group>
 
-                <Button primary>Save</Button>
-            </Form>
+                        <h2>Stock</h2>
+                        <Form.Group>
+                            <Form.Input width={6}
+                                        label="Quantity in stock"
+                                        type="number"
+                                        name="totalAvailable"
+                                        required
+                                        placeholder="47">
+                                <input type="number" min={0}/>
+                            </Form.Input>
+                            <Form.Input width={6}
+                                        label="Quantity allowed per request"
+                                        type="number"
+                                        name="maxRequestQty"
+                                        required
+                                        placeholder="6">
+                                <input type="number" min={0}/>
+                            </Form.Input>
+                        </Form.Group>
+                        {/*<Form.TextArea width={6}*/}
+                        {/*               label="Serial numbers (one per line)"*/}
+                        {/*               name="item-serial-nums"*/}
+                        {/*               rows={6}*/}
+                        {/*               placeholder="2873-813"/>*/}
+
+                        <h2>Checkout Controls</h2>
+
+                        <Popup inverted={true} trigger={<Form.Checkbox name="returnRequired"
+                                                                       label="Return required"
+                                                                       onChange={this.handleInputChangeCheckbox}
+                                                                       defaultChecked/>}
+                               content="Whether users who check out this item are expected to return it"/>
+                        <Popup inverted={true} trigger={<Form.Checkbox label="Approval required"
+                                                                       name="approvalRequired"
+                                                                       onChange={this.handleInputChangeCheckbox}
+                                                                       defaultChecked/>}
+                               content="Whether hardware checkout staff must approve requests for this item"/>
+                        <Popup inverted={true} trigger={<Form.Checkbox label="Hidden"
+                                                                       name="hidden"
+                                                                       onChange={this.handleInputChangeCheckbox}/>}
+                               content="Whether to hide this item on the public list of hardware"/>
+
+                        <Button primary type="submit">Create item</Button>
+                        {console.log(loading, called, error, data)}
+                    </Form>
+                )}
+            </Mutation>
         );
-
     }
 }
 
-export default ItemEditForm;
+export default withToastManager(ItemEditForm);
