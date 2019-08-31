c54d97d4601788061c4cd6f8867bc31fba70627a
Evan Strat <evan10s@users.noreply.github.com>
2019-08-31 15:25:00 -0400

    Converted to SFC

--- client/src/components/inventory/PlaceholderItem.tsx
+++ client/src/components/inventory/PlaceholderItem.tsx
@@ -1,23 +1,21 @@
-import React, {Component} from "react";
+import React from "react";
 import {Placeholder} from "semantic-ui-react";
 
-class PlaceholderItem extends Component {
-    public render() {
-        return (
-            <Placeholder>
-                <Placeholder.Header image>
-                    <Placeholder.Line/>
-                    <Placeholder.Line/>
-                </Placeholder.Header>
-                <Placeholder.Paragraph>
-                    <Placeholder.Line/>
-                    <Placeholder.Line/>
-                    <Placeholder.Line/>
-                    <Placeholder.Line/>
-                </Placeholder.Paragraph>
-            </Placeholder>
-        );
-    }
+function PlaceholderItem() {
+    return (
+        <Placeholder>
+            <Placeholder.Header image>
+                <Placeholder.Line/>
+                <Placeholder.Line/>
+            </Placeholder.Header>
+            <Placeholder.Paragraph>
+                <Placeholder.Line/>
+                <Placeholder.Line/>
+                <Placeholder.Line/>
+                <Placeholder.Line/>
+            </Placeholder.Paragraph>
+        </Placeholder>
+    );
 }
 
 export default PlaceholderItem;
