ca62c70bffb6ed13ae9f1dc4672df5fb0f42fc80
Evan Strat <evan10s@users.noreply.github.com>
2019-09-22 03:08:45 -0400

    Finish issues with deploying migrations

--- server/src/knexfile.ts
+++ server/src/knexfile.ts
@@ -4,23 +4,27 @@ const fs = require("fs");
 
 // Some duplicated code here from common.ts, but we can't use import in this file and we only need a couple values for knex
 const path = require("path");
-let config;
-try {
-    config = JSON.parse(fs.readFileSync(path.resolve(__dirname, "./config", "config.json"), "utf8"));
-} catch (err) {
-    if (err.code !== "ENOENT") {
-        throw err;
-    }
-}
+
 let URL;
 let NODE_ENV = "development";
-
-if (config.server) {
-    if (config.server.postgresURL) {
-        URL = config.server.postgresURL;
+if (!process.env.PRODUCTION) {
+    let config;
+    try {
+        config = JSON.parse(fs.readFileSync(path.resolve(__dirname, "./config", "config.json"), "utf8"));
+    } catch (err) {
+        if (err.code !== "ENOENT") {
+            throw err;
+        }
     }
-    if (config.server.isProduction) {
-        NODE_ENV = "production";
+
+
+    if (config.server) {
+        if (config.server.postgresURL) {
+            URL = config.server.postgresURL;
+        }
+        if (config.server.isProduction) {
+            NODE_ENV = "production";
+        }
     }
 }
 
