120c688eaade2892cf0f4eaf612ecaacf9ee92fb
Evan Strat <evan10s@users.noreply.github.com>
2019-09-22 13:40:05 -0400

    Fix issues with deploying migrations (#73)
    * Finish issues with deploying migrations
    
    * globally install knex cli
    
    * trying another thing
    
    * add some debug statements
    
    * Try removing env-specificity from knexfile since we don't need it
    
    * Finishing touches

--- server/src/knexfile.ts
+++ server/src/knexfile.ts
@@ -4,23 +4,20 @@ const fs = require("fs");
 
 // Some duplicated code here from common.ts, but we can't use import in this file and we only need a couple values for knex
 const path = require("path");
-let config;
-try {
-    config = JSON.parse(fs.readFileSync(path.resolve(__dirname, "./config", "config.json"), "utf8"));
-} catch (err) {
-    if (err.code !== "ENOENT") {
-        throw err;
-    }
-}
-let URL;
-let NODE_ENV = "development";
 
-if (config.server) {
-    if (config.server.postgresURL) {
-        URL = config.server.postgresURL;
+let URL;
+if (!(process.env.PRODUCTION && process.env.PRODUCTION.toLowerCase() === "true")) {
+    let config;
+    try {
+        config = JSON.parse(fs.readFileSync(path.resolve(__dirname, "./config", "config.json"), "utf8"));
+    } catch (err) {
+        if (err.code !== "ENOENT") {
+            throw err;
+        }
     }
-    if (config.server.isProduction) {
-        NODE_ENV = "production";
+
+    if (config.server && config.server.postgresURL) {
+        URL = config.server.postgresURL;
     }
 }
 
@@ -28,16 +25,12 @@ if (process.env.POSTGRES_URL) {
     URL = process.env.POSTGRES_URL;
 }
 
-if (process.env.PRODUCTION && process.env.PRODUCTION.toLowerCase() === "true") {
-    NODE_ENV = "production";
-}
-
 const migrations = {
     tableName: "knex_migrations",
     directory: path.normalize(path.join(__dirname, "/migrations"))
 };
 
-module.exports[NODE_ENV] = {
+module.exports = {
     client: "pg",
     connection: URL,
     migrations
