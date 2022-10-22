02c4fef3ff3347abe13bfc7f3c7f72d44871e8ab
Edmund Xin <marxin@tesla.com>
2022-10-21 19:31:55 -0700

    Add name to request

--- client/src/components/admin/desk/submitted/SubmittedCard.tsx
+++ client/src/components/admin/desk/submitted/SubmittedCard.tsx
@@ -4,9 +4,12 @@ import { Box, Flex, Heading, IconButton, Text, Tooltip } from "@chakra-ui/react"
 import ReactTimeago from "react-timeago";
 import { DraggableProvided } from "react-beautiful-dnd";
 import { CheckIcon, DeleteIcon } from "@chakra-ui/icons";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { apiUrl, LoadingScreen, Service } from "@hex-labs/core";
 
-import { Request } from "../../../../types/Request";
 import { generateBadge } from "./SubmittedTable";
+import { Request } from "../../../../types/Request";
 
 interface SubmittedCardProps {
   provided: DraggableProvided;
@@ -28,38 +31,49 @@ const noIssues = (
   </Flex>
 );
 
-const SubmittedCard = ({ provided, request }: SubmittedCardProps) => (
-  <Flex
-    ref={provided.innerRef}
-    {...provided.draggableProps}
-    {...provided.dragHandleProps}
-    rounded={8}
-    p={4}
-    shadow="md"
-    minH={32}
-    bgColor="white"
-    mb={4}
-    justify="space-between"
-    alignItems="center"
-  >
-    <Flex gap={1} flexDir="column">
-      <Flex gap={2}>
-        <Heading as="h4" size="md">
-          {request.item.name}
-        </Heading>
-        <Text color="gray.500">{`Qty: ${request.quantity}`}</Text>
+const SubmittedCard = ({ provided, request }: SubmittedCardProps) => {
+  const { data, isLoading } = useQuery(["user"], () =>
+    axios.get(apiUrl(Service.USERS, `/users/${request.user}`))
+  );
+
+  if (isLoading) {
+    return <LoadingScreen />;
+  }
+
+  return (
+    <Flex
+      ref={provided.innerRef}
+      {...provided.draggableProps}
+      {...provided.dragHandleProps}
+      rounded={8}
+      p={4}
+      shadow="md"
+      minH={32}
+      bgColor="white"
+      mb={4}
+      justify="space-between"
+      alignItems="center"
+    >
+      <Flex gap={1} flexDir="column">
+        <Heading size="lg">{`${data?.data.name.first} ${data?.data.name.last}`}</Heading>
+        <Flex gap={2}>
+          <Heading as="h4" size="md">
+            {request.item.name}
+          </Heading>
+          <Text color="gray.500">{`Qty: ${request.quantity}`}</Text>
+        </Flex>
+        <Text color="gray.500">{request.item.description}</Text>
+        {request.item.totalAvailable >= request.quantity
+          ? noIssues
+          : noStockWarning(request.item.totalAvailable)}
+        <ReactTimeago date={request.createdAt} />
+        <Box>{generateBadge(request.status)}</Box>
       </Flex>
-      <Text color="gray.500">{request.item.description}</Text>
-      {request.item.totalAvailable >= request.quantity
-        ? noIssues
-        : noStockWarning(request.item.totalAvailable)}
-      <ReactTimeago date={request.createdAt} />
-      <Box>{generateBadge(request.status)}</Box>
+      <Tooltip label="Delete Request">
+        <IconButton aria-label="delete" icon={<DeleteIcon />} color="red.400" variant="ghost" />
+      </Tooltip>
     </Flex>
-    <Tooltip label="Delete Request">
-      <IconButton aria-label="delete" icon={<DeleteIcon />} color="red.400" variant="ghost" />
-    </Tooltip>
-  </Flex>
-);
+  );
+};
 
 export default SubmittedCard;
